d04a7e021c17219e05942b124d094f1c8c93dd9a
dispellizer <dispellizer@naver.com>
2020-03-24 21:56:18 +0900

    2.0 Hello World part One

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 import { join } from "path";
 import express from "express";
 import socketIO from "socket.io";
+import logger from "morgan";
 
 const PORT = 5000;
 const app = express();
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
+app.use(logger("dev"));
 app.use(express.static(join(__dirname, "static")));
 app.get("/", (req, res) => res.render("home"));
 
@@ -14,5 +16,13 @@ const handleListening = () =>
 
 const server = app.listen(PORT, handleListening);
 
-const io = socketIO(server);
+const io = socketIO.listen(server);
 // socketIO는 server와 client가 동시에 될수 있다.
+
+let sockets = [];
+// 연결된 소켓 id를 보기위한 빈 배열
+
+io.on("connection", socket => sockets.push(socket.id));
+// 연결될때마다 sockets에 socket.id값을 push해준다
+
+setInterval(() => console.log(sockets), 1000);
--- src/static/index.js
+++ src/static/index.js
@@ -1,1 +1,3 @@
-hello;
+// eslint-disable-next-line no-undef
+io("/");
+// 프론트에 접속하면 자동으로 연결하기 위해서 작성
