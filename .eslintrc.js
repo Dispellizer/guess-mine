3c71a6c50a4cc0845f6b59ad518ed7b49cf11aaf
dispellizer <dispellizer@naver.com>
2020-03-25 10:54:30 +0900

    3.0 Gulp is Awesome part One

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,10 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    node: true
-  },
-  extends: ["eslint:recommended", "plugin:prettier/recommended"],
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: "module"
-  },
+  extends: ["airbnb-base", "plugin:prettier/recommended"],
   rules: {
-    "no-console": "off"
+    "no-console": "off",
+    "linebreak-style": 0
+  },
+  env: {
+    browser: true
   }
 };
