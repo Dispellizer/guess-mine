2aeb61d5a2989dc1dd3e65b590c067b35bc2dab8
dispellizer <dispellizer@naver.com>
2020-03-27 10:54:36 +0900

    6.0 Realtime Painting part One

--- src/socketController.js
+++ src/socketController.js
@@ -8,12 +8,18 @@ const socketController = socket => {
     broadcast(events.newUser, { nickname });
   });
 
-  socket.on(events.disconnect, () => {
-    broadcast(events.disconnected, { nickname: socket.nickname });
-  });
+  socket.on(events.disconnect, () =>
+    broadcast(events.disconnected, { nickname: socket.nickname })
+  );
 
-  socket.on(events.sendMsg, ({ message }) => {
-    broadcast(events.newMsg, { message, nickname: socket.nickname });
+  socket.on(events.sendMsg, ({ message }) =>
+    broadcast(events.newMsg, { message, nickname: socket.nickname })
+  );
+  socket.on(events.beginPath, ({ x, y }) =>
+    broadcast(events.beganPath, { x, y })
+  );
+  socket.on(events.strokePath, ({ x, y }) => {
+    broadcast(events.strokedPath, { x, y });
   });
 };
 
